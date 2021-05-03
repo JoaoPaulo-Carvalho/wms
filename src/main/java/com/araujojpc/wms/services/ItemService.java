19529db88aa9657060ca772347658a9d210c7f10
João Paulo de Carvalho Araújo <araujo.jpc@outlook.com>
2021-05-03 17:48:57 -0300

    Feature: Método GET por ID para Item, sem tratamento de erro

--- src/main/java/com/araujojpc/wms/services/ItemService.java
+++ src/main/java/com/araujojpc/wms/services/ItemService.java
@@ -1,6 +1,7 @@
 package com.araujojpc.wms.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,5 +17,10 @@ public class ItemService {
 
 	public List<Item> findAll() {
 		return repository.findAll();
+	}
+	
+	public Item findById(Long id) {
+		Optional<Item> obj = repository.findById(id);
+		return obj.get();
 	}
 }
