628420287805f9c4a9666ab2cb70dc43209a6bb8
João Paulo de Carvalho Araújo <araujo.jpc@outlook.com>
2021-05-05 18:56:06 -0300

    Feature: Movement GET methods (without unit tests)

--- src/main/java/com/araujojpc/wms/resources/MovementResource.java
+++ src/main/java/com/araujojpc/wms/resources/MovementResource.java
@@ -1,12 +1,19 @@
 package com.araujojpc.wms.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.araujojpc.wms.entities.Movement;
 import com.araujojpc.wms.services.MovementService;
@@ -22,5 +29,30 @@ public class MovementResource {
 	private ResponseEntity<List<Movement>> findAll() {
 		List<Movement> list = service.findAll();
 		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	private ResponseEntity<Movement> findById(@PathVariable Long id) {
+		Movement obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+	@PostMapping
+	private ResponseEntity<Movement> insert(@RequestBody Movement obj) {
+		service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	private ResponseEntity<Movement> delete(@PathVariable Long id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	private ResponseEntity<Movement> update(@PathVariable Long id, @RequestBody Movement obj) {
+		service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
--- src/main/java/com/araujojpc/wms/services/MovementService.java
+++ src/main/java/com/araujojpc/wms/services/MovementService.java
@@ -1,20 +1,59 @@
 package com.araujojpc.wms.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.araujojpc.wms.entities.Movement;
+import com.araujojpc.wms.repositories.ItemMovementRepository;
 import com.araujojpc.wms.repositories.MovementRepository;
+import com.araujojpc.wms.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class MovementService {
 
 	@Autowired
 	private MovementRepository repository;
+	
+	@Autowired
+	private ItemMovementRepository itemMovementRepository;
 
 	public List<Movement> findAll() {
 		return repository.findAll();
+	}
+
+	public Movement findById(Long id) {
+		Optional<Movement> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+	}
+
+	public void insert(Movement obj) {
+		repository.save(obj);
+		obj.getItems().forEach(i -> i.setMovement(obj));
+		itemMovementRepository.saveAll(obj.getItems());
+	}
+
+	public void delete(Long id) {
+		Movement entity = repository.getOne(id);
+		itemMovementRepository.deleteAll(entity.getItems());
+		repository.deleteById(id);
+	}
+
+	public Movement update(Long id, Movement obj) {
+		Movement entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+	}
+
+	private void updateData(Movement entity, Movement obj) {
+		entity.setDescription(obj.getDescription());
+		entity.getItems().clear();
+		obj.getItems().forEach(item -> {
+			entity.getItems().add(item);
+			item.setMovement(entity);
+		});
+		itemMovementRepository.saveAll(obj.getItems());
 	}
 }
